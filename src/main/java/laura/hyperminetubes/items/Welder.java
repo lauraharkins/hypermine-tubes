2c00af04f7402c9d33145c436482009dd0c6cb79
lauraharkins <laura_harkins@hotmail.com>
2023-05-18 22:24:24 +0100

    Glowing Item - not custom png.

--- src/main/java/laura/hyperminetubes/items/Welder.java
+++ src/main/java/laura/hyperminetubes/items/Welder.java
@@ -1,14 +1,23 @@
 package laura.hyperminetubes.items;
 
+import org.jetbrains.annotations.Nullable;
+
+import eu.pb4.polymer.core.api.item.PolymerItem;
+import eu.pb4.polymer.core.api.item.PolymerItemUtils;
+import laura.hyperminetubes.ExampleMod;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
-public class Welder extends Item {
+public class Welder extends Item implements PolymerItem {
      
     public Welder(Settings settings) {
         super(settings);
@@ -18,5 +27,17 @@ public class Welder extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
         playerEntity.playSound(SoundEvents.BLOCK_CANDLE_EXTINGUISH, 1.0F, 1.0F);
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
+    }
+
+    @Override
+    public Item getPolymerItem(ItemStack arg0, @Nullable ServerPlayerEntity arg1) {
+        return Items.FLOWER_BANNER_PATTERN;
+    }
+
+    @Override
+    public ItemStack getPolymerItemStack(ItemStack itemStack, TooltipContext context, ServerPlayerEntity player) {
+        ItemStack out = PolymerItemUtils.createItemStack(itemStack, context, player);
+        out.addEnchantment(Enchantments.LURE, 0);
+        return out;
     }
 }
